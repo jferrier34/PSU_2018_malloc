09aa4e882345e7673ded9d84d98d5eecd5933f7b
jferrier <jferrier@localhost.localdomain>
2019-01-31 09:04:16 -0500

    [added] first criterion test

--- test/test_criterion_malloc.c
+++ test/test_criterion_malloc.c
@@ -7,6 +7,10 @@
 
 #include <criterion/criterion.h>
 
-Test()
+Test(get_first_page)
 {
+        char *my_test = (char *) malloc(10 * sizeof(char));
+        free(my_test);
+
+        cr_assert(true, "Every thinks is ok");
 }
